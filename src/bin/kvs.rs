abcef659f6b4c98ca9feab7cd3588d02b2fb8bdb
shawn <coolrainerseth@gmail.com>
2023-05-29 15:52:41 +0800

    refactor

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,60 +1,59 @@
 use std::process::exit;
 
-use clap::{Arg, Command};
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(
+    name = env!("CARGO_PKG_NAME"),
+    bin_name = env!("CARGO_PKG_NAME"),
+    author = env!("CARGO_PKG_AUTHORS"),
+    version = env!("CARGO_PKG_VERSION"),
+    about = env!("CARGO_PKG_DESCRIPTION"),
+)]
+pub enum Opts {
+    Get(GetArgs),
+    Set(SetArgs),
+    #[command(name = "rm")]
+    Remove(RmArgs),
+}
+
+#[derive(clap::Args)]
+#[command(about = "Get the string value of a given string key")]
+pub struct GetArgs {
+    #[arg(help = "A string key")]
+    key: String,
+}
+
+#[derive(clap::Args)]
+#[command(about = "Set the value of a string key to a string")]
+pub struct SetArgs {
+    #[arg(help = "A string key")]
+    key: String,
+    #[arg(help = "The string value of the key")]
+    value: String,
+}
+
+#[derive(clap::Args)]
+#[command(about = "Remove a given key")]
+pub struct RmArgs {
+    #[arg(help = "A string key")]
+    key: String,
+}
 
 fn main() {
-    let _store = kvs::KvStore::new();
-    let cmd = Command::new(env!("CARGO_PKG_NAME"))
-        .version(env!("CARGO_PKG_VERSION"))
-        .author(env!("CARGO_PKG_AUTHORS"))
-        .subcommand(
-            Command::new("set")
-                .about("Set the value of a string key to a string")
-                .arg(Arg::new("key").help("A string key").required(true))
-                .arg(
-                    Arg::new("value")
-                        .help("The string value of the key")
-                        .required(true),
-                ),
-        )
-        .subcommand(
-            Command::new("get")
-                .about("Get the string value of a given string key")
-                .arg(Arg::new("key").help("A string key").required(true)),
-        )
-        .subcommand(
-            Command::new("rm")
-                .about("Remove a given key")
-                .arg(Arg::new("key").help("A string key").required(true)),
-        );
-
-    let matches = cmd.get_matches();
-    match matches.subcommand() {
-        Some(("set", _args)) => {
-            // let key = args.get_one::<String>("key").unwrap();
-            // let value = args.get_one::<String>("value").unwrap();
-            // let mut store = kvs::KvStore::new();
-            // store.set(key.to_owned(), value.to_owned());
+    let opts = Opts::parse();
+    match opts {
+        Opts::Get(_args) => {
             eprintln!("unimplemented");
             exit(-1);
         }
-        Some(("get", _args)) => {
-            // let key = args.get_one::<String>("key").unwrap();
-            // let store = kvs::KvStore::new();
-            // match store.get(key.to_owned()) {
-            //     Some(value) => println!("{}", value),
-            //     None => println!("nil"),
-            // }
+        Opts::Set(_args) => {
             eprintln!("unimplemented");
             exit(-1);
         }
-        Some(("rm", _args)) => {
-            // let key = args.get_one::<String>("key").unwrap();
-            // let mut store = kvs::KvStore::new();
-            // store.remove(key.to_owned());
+        Opts::Remove(_args) => {
             eprintln!("unimplemented");
             exit(-1);
         }
-        _ => unimplemented!(),
     }
 }
